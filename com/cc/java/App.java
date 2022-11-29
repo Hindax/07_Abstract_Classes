b1f170b8e26a259d2b617ccd3be5cec8a7e9e000
Hindax <haar162@more2learn.de>
2022-11-29 12:20:54 +0100

    are method

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,13 +3,13 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+
+        // new Shape();
     }
 
     public static void output(String outputStr) {
         System.out.println(outputStr);
     }
-
 
 
 }
